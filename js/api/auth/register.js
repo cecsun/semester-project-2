a4d65cec2a861dca5f14a639e2d133d22c9da218
cecsun <ceciliesunde94@gmail.com>
2024-02-07 20:16:55 +0100

    Register

--- js/api/auth/register.js
+++ js/api/auth/register.js
@@ -1,43 +1,53 @@
-import { REGISTER_API_URL } from "../constants.mjs";
-import { fetcher } from "../../fetcher.js";
+import { REGISTER_API_URL } from "/js/api/constants.mjs";
+import { fetcher } from "/js/fetcher.js";
+import { validateEmail, validatePassword } from "/js/utils/form-validation.mjs";
 
 const form = document.querySelector('#register-form');
-const name = document.querySelector('#form-name');
-const email = document.querySelector('#form-email');
-const password = document.querySelector('#form-password');
 
 async function registerUser(user) {
     const postBody = JSON.stringify(user);
-    const myData = await fetcher(REGISTER_API_URL, {
+    const userRegisterData = await fetcher(REGISTER_API_URL, {
         method: 'POST',
         body: postBody,
     });
-    console.log(myData);
+    if (userRegisterData.errors) {
+        alert(userRegisterData.errors[0].message);
+        return;
+    }
 }
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     const user = {
-        name: name.value,
-        email: email.value,
-        password: password.value,
+        name: document.forms["register-form"]["form-name"].value,
+        email: document.forms["register-form"]["form-email"].value,
+        password: document.forms["register-form"]["form-password"].value,
     };
+    if (!validateRegisterForm(user)) {
+        return;
+    }
     registerUser(user);
 });
 
-function validateEmail() {
-    const emailInput = document.getElementById('form-email');
-    const domain = ["stud.noroff.no"];
-    var regex = new RegExp('^[a-zA-Z0-9._-]+@' + domain + '$');
-    
-    if (regex.test(emailInput)) {
-        alert('Email is valid');
-        return true;
-    } else {
-        alert('Email must be of type @stud.noroff.no');
+
+function validateRegisterForm(user) {
+    if (user.name.length <= 5) {
+      alert("Username must be more than 5 characters");
+      return false;
+    }
+  
+    if (user.name.split(" ").length >= 2) {
+      alert("Username cannot contain spaces ");
+      return false;
+    }
+
+    if (!validateEmail(user.email)) {
+        alert('Email must be of type @stud.noroff.no or @noroff.no');
+        return false; 
+    }
+  
+    if (!validatePassword(user.password)) {
+        alert("Password must be at least 8 characters");
         return false;
     }
 }
-
-
-
