25980b8a92220b23909bc89a4f20b964c07934f8
cecsun <ceciliesunde94@gmail.com>
2024-02-07 20:17:50 +0100

    Fetcher

--- js/fetcher.js
+++ js/fetcher.js
@@ -1,20 +1,53 @@
-import { getFromLocalStorage } from "./utils/local-storage.js";
+// import { getFromLocalStorage } from "./utils/local-storage.js";
 
-export async function fetcher(url, options) {
-    try {
-        const token = getFromLocalStorage('token');
-        const options_with_auth = {
-            ...options,
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${token}`,
-            },
+export async function fetcher(url, options = { method: "GET" }, useToken = false) {
+    options = {
+        ...options,
+        headers: {
+            ...options.headers,
+            'Content-Type': 'application/json',
+        },
     }
 
-    const response = await fetch(url, options_with_auth);
-    const data = await response.json();
-    return data;
+    try {
+        if (useToken) {
+            const token = localStorage.getItem('accessToken');
+            options = {
+                ...options,
+                headers: {
+                    ...options.headers,
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        }
+        const response = await fetch(url, options);
+        const json = await response.json();
+        return json;
     } catch (error) {
         console.error('Error with API call', error);
     }
 }
+
+
+
+
+
+
+// export async function fetcher(url, options) {
+//     try {
+//         const token = getFromLocalStorage('token');
+//         const options_with_auth = {
+//             ...options,
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 Authorization: `Bearer ${token}`,
+//             },
+//     }
+
+//     const response = await fetch(url, options_with_auth);
+//     const data = await response.json();
+//     return data;
+//     } catch (error) {
+//         console.error('Error with API call', error);
+//     }
+// }
