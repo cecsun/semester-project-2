352b718b6595ed4e65819a203c704625e9a26ee5
cecsun <ceciliesunde94@gmail.com>
2024-02-13 20:59:40 +0100

    Search

---
+++ js/search.js
@@ -0,0 +1,16 @@
+import { displayListings } from "/js/api/listings/listings.js";
+import { getListings } from "/js/api/listings/storage.mjs";
+
+const viewListings = document.getElementById('display-listings');
+
+const searchFormSubmit = document.getElementById('search-submit');
+searchFormSubmit.addEventListener('click', async (event) => {
+    event.preventDefault();
+    const search = document.forms['search-form']['search'].value;
+    viewListings.innerHTML = '';
+    let listings = getListings();
+    console.log(listings)
+    let isLoggedin = localStorage.getItem('accessToken') !== null;
+    let filterListingsHandler = (listing) => listing.title.toLowerCase().startsWith(search.toLowerCase().trim());
+    displayListings(listings, filterListingsHandler, isLoggedin);
+})
