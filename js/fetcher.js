2d6a8f66cfcec8cd9f06cf0f2c77c456588db269
cecsun <ceciliesunde94@gmail.com>
2024-02-13 20:57:11 +0100

    Updated fetcher

--- js/fetcher.js
+++ js/fetcher.js
@@ -29,25 +29,3 @@ export async function fetcher(url, options = { method: "GET" }, useToken = false
 }
 
 
-
-
-
-
-// export async function fetcher(url, options) {
-//     try {
-//         const token = getFromLocalStorage('token');
-//         const options_with_auth = {
-//             ...options,
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 Authorization: `Bearer ${token}`,
-//             },
-//     }
-
-//     const response = await fetch(url, options_with_auth);
-//     const data = await response.json();
-//     return data;
-//     } catch (error) {
-//         console.error('Error with API call', error);
-//     }
-// }
