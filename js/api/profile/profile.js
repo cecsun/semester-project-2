e50819c7e61c9753b3f5b672d441bc3a9d0ebe21
cecsun <ceciliesunde94@gmail.com>
2024-02-13 20:59:55 +0100

    Logout

--- js/api/profile/profile.js
+++
@@ -1,8 +0,0 @@
-import { logout } from "../../utils/local-storage.js";
-
-const logoutButton = document.querySelector('#logout-button');
-
-logoutButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    logout();
-});
