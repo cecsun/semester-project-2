f42b21f59c0b228da6b7efefb69bdfdd8cb2857a
cecsun <ceciliesunde94@gmail.com>
2024-02-21 14:00:29 +0100

    Added profile api url

--- js/api/constants.mjs
+++ js/api/constants.mjs
@@ -2,3 +2,4 @@ const BASE_API_URL = 'https://api.noroff.dev/api/v1';
 export const REGISTER_API_URL = `${BASE_API_URL}/auction/auth/register`;
 export const LOGIN_API_URL = `${BASE_API_URL}/auction/auth/login`;
 export const LISTINGS_API_URL = `${BASE_API_URL}/auction/listings`;
+export const PROFILE_API_URL = `${BASE_API_URL}/auction/profiles`;
