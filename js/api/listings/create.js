eab666accd3c8f186b05403ed984030e65fda25d
cecsun <ceciliesunde94@gmail.com>
2024-02-24 15:41:54 +0100

    Updated create form for user and for unregistered user

--- js/api/listings/create.js
+++ js/api/listings/create.js
@@ -1,27 +1,42 @@
-import { fetcher } from "/js/fetcher.js";
+import { fetcher } from "../../fetcher.js";
 
 const submitNewListing = document.querySelector('#submit-new-listing');
 
 async function createListing() {
     const BASE_API_URL = 'https://api.noroff.dev/api/v1';
     const LISTINGS_API_URL = `${BASE_API_URL}/auction/listings`;
+    const media = document.forms["new-listing-form"]["inputMedia"].value;
     const listing = {
         title: document.forms["new-listing-form"]["inputTitle"].value,
         body: document.forms["new-listing-form"]["inputDescription"].value,
-        media: document.forms["new-listing-form"]["inputMedia"].value,
+        media: media ? [media] : [],
         endsAt: document.forms["new-listing-form"]["inputEndsAt"].value,
     }
-    console.log(listing.endsAt);
+
     const response = await fetcher(
         LISTINGS_API_URL, 
         { method: 'POST', body: JSON.stringify(listing) }, 
+        true,
     );
+
+    if (response.errors) {
+        alert(response.errors[0].message);
+        return;
+    }
     return response;
 }
 
-submitNewListing.addEventListener('click', async (event) => {
-    event.preventDefault();
-    const response = await createListing();
-    console.log(response);
-    // location.reload();
-});
+function main() {
+    const submitNewListingForm = document.getElementById('new-listing-form');
+    if (!localStorage.getItem('accessToken')) {
+        submitNewListingForm.innerHTML = "User must be logged in to create a listing. Please log in.";
+        return;
+    }
+    submitNewListing.addEventListener('click', async (event) => {
+        event.preventDefault();
+        const response = await createListing();
+        console.log(response);
+    });
+}
+
+main();
