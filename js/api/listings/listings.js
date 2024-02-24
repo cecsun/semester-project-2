393b80b4c0c6393a7c0f88dbeea0657cd2ccecb0
cecsun <ceciliesunde94@gmail.com>
2024-02-24 15:42:26 +0100

    Updated deadline date

--- js/api/listings/listings.js
+++ js/api/listings/listings.js
@@ -12,10 +12,15 @@ function sortCallback(a, b) {
     return 0;
   }
 
+function filterExpired(listing) {
+    return new Date(listing.endsAt) > new Date();
+}
+
 export async function displayListings(listings, filterCallback, isAuthorized = false) {
     listingsContainer.innerHTML = '';
     listings
     .filter(filterCallback)
+    .filter(filterExpired)
     .sort(sortCallback)
     .forEach((listing) => {
         const currentListing = generateListingHtml(listing, isAuthorized);
