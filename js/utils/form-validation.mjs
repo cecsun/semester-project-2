3ba4f70ca775890df1f97452075c110af3d8d9c4
cecsun <ceciliesunde94@gmail.com>
2024-02-24 15:44:29 +0100

    Bid validation

--- js/utils/form-validation.mjs
+++ js/utils/form-validation.mjs
@@ -1,3 +1,5 @@
+import { getFromLocalStorage } from "./local-storage.mjs";
+
 export function validateEmail(email) {
     const email_regex = /^[\w\-.]+@(stud.)?noroff.no$/;
     return email_regex.test(email);
@@ -5,4 +7,13 @@ export function validateEmail(email) {
 
 export function validatePassword(password) {
     return password.length >= 8;
+}
+
+
+export function validateBid(bid) {
+    if (bid < 0) {
+        return false;
+    }
+    const number_regex = /^\d*$/;
+    return number_regex.test(bid);
 }
