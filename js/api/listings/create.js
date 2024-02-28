ad038549765234a0e52f273fd26475a3bf7b4adb
cecsun <ceciliesunde94@gmail.com>
2024-02-28 21:08:57 +0100

    Added media gallery

--- js/api/listings/create.js
+++ js/api/listings/create.js
@@ -5,11 +5,17 @@ const submitNewListing = document.querySelector('#submit-new-listing');
 async function createListing() {
     const BASE_API_URL = 'https://api.noroff.dev/api/v1';
     const LISTINGS_API_URL = `${BASE_API_URL}/auction/listings`;
-    const media = document.forms["new-listing-form"]["inputMedia"].value;
+    let medias = [];
+    const media1 = document.forms["new-listing-form"]["inputMedia1"].value;
+    const media2 = document.forms["new-listing-form"]["inputMedia2"].value;
+    const media3 = document.forms["new-listing-form"]["inputMedia3"].value;
+    if (media1) medias.push(media1);
+    if (media2) medias.push(media2);
+    if (media3) medias.push(media3);
     const listing = {
         title: document.forms["new-listing-form"]["inputTitle"].value,
         body: document.forms["new-listing-form"]["inputDescription"].value,
-        media: media ? [media] : [],
+        media: medias,
         endsAt: document.forms["new-listing-form"]["inputEndsAt"].value,
     }
 
